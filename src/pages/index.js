b64049bf1886e772834d2baf1203b1a460bfe824
D-Big-B <prjjwlbhatt@gmail.com>
2022-01-22 16:29:21 +0530

    First Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
 import React from "react"
+import Footer from "../components/footer/Footer"
+import Header from "../components/header/Header"
+import Hero from "../components/hero/Hero"
+import About from "../components/about/About"
+import Events from "../components/events/Events"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <About />
+      <Events />
+      <Footer />
+    </div>
+  )
 }
